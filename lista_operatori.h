813db6f285f71434b5d00d330ce771f31f342af9
Andrea Peretti <and.xet@gmail.com>
2012-04-17 11:26:53 +0200

    Inizio implementazione parte memoria condivisa e semafori. Commit necessario per merge

---
+++ lista_operatori.h
@@ -0,0 +1,14 @@
+//
+//  lista_operatori.h
+//  helpdesk
+//
+//  Created by Andrea Peretti on 13/04/12.
+//  Copyright (c) 2012 Università degli studi di Torino. All rights reserved.
+//
+
+#ifndef helpdesk_lista_operatori_h
+#define helpdesk_lista_operatori_h
+
+
+
+#endif
